de34336350588ff949551483ae58c021c7649477
Sven Gabriel <svengabr@googlemail.com>
2024-04-26 09:20:54 +0200

    changes on privacy page

--- src/assets/documents/generateEventQRCodes.bat
+++
@@ -1,2 +0,0 @@
-if "%1" NEQ "" cwa-event-qr-code file --csv %1 --dest qr-codes-results
-if "%1" ==  "" cwa-event-qr-code file --csv tables.csv --dest qr-codes-results
--- src/assets/documents/setupEventQRCodeGenerator.bat
+++
@@ -1,5 +0,0 @@
-cd /d %USERPROFILE%/Downloads	
-mkdir event-qr-codes	
-cd event-qr-codes	
-mkdir qr-codes-results
-npm install cwa-event-qr-code --global
